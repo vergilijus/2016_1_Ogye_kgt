ec39497e625f31c72d65e6cf7133b7251c216209
Ruslan <vergilijus@ya.ru>
2016-03-04 02:16:07 +0300

    Поправил форматирование где нашел.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     errorHandler = require('errorhandler'),
     app = express(),
-	proxy = require('express-http-proxy');
+    proxy = require('express-http-proxy');
 
 var HOSTNAME = 'localhost',
     PORT = 8080,
@@ -11,22 +11,22 @@ var i = 0;
 
 app.use(function (req, res, done) {
     var date = new Date();
-	console.log('[' + date.toLocaleString() + '] [Request no.' + i++ + ']');
-	done();
+    console.log('[' + date.toLocaleString() + '] [Request no.' + i++ + ']');
+    done();
 });
 
 app
-	.use('/', express.static(PUBLIC_DIR))
-	.use(errorHandler());
+    .use('/', express.static(PUBLIC_DIR))
+    .use(errorHandler());
 
 app.listen(PORT, function () {
-	console.log("Simple static server showing %s listening at http://%s:%s", PUBLIC_DIR, HOSTNAME, PORT);
+    console.log("Simple static server showing %s listening at http://%s:%s", PUBLIC_DIR, HOSTNAME, PORT);
 });
 
 
 app.use('/proxy', proxy('http://vk.com', {
-	forwardPath: function(req, res) {
-		console.log(1234);
-		return require('url').parse(req.url).path;
-	}
+    forwardPath: function (req, res) {
+        console.log(1234);
+        return require('url').parse(req.url).path;
+    }
 }));
