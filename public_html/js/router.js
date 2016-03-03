1993b5abab5afd95c58a61863ee187d71f007fcd
chongkal <hommequirit.seng@gmail.com>
2016-03-03 22:39:36 +0300

    BE

--- public_html/js/router.js
+++ public_html/js/router.js
@@ -5,12 +5,14 @@ define(function(require){
         loginView = require('views/login'),
         gameView = require('views/game'),
         scoreboardView = require('views/scoreboard');
+        registrationView = require('views/registration');
 
     var Router = Backbone.Router.extend({
         routes: {
             'scoreboard': 'scoreboardAction',
             'game': 'gameAction',
             'login': 'loginAction',
+            'registration': 'registrationAction',
             '*default': 'defaultActions'
         },
         defaultActions: function () {
@@ -28,6 +30,10 @@ define(function(require){
         loginAction: function () {
             console.log('the #login route');
             $('#page').html(loginView.render().$el);
+        },
+        registrationAction: function () {
+            console.log('the #registration route');
+            $('#page').html(registrationView.render().$el);
         }
     });
 
